1ca9fb3a4308af451431c63d0f5cb27abcbdcc1c
Kelares <michalowski.jb@gmail.com>
2026-03-23 13:38:46 +0100

    Some executable added

---
+++ HyperFisher/split_mnist_mlp.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -p GPU # partition (queue)
+#SBATCH -N 1 # number of nodes
+#SBATCH -t 0-36:00 # time (D-HH:MM)
+#SBATCH -o slurm/%j.out # STDOUT
+#SBATCH -e slurm/%j.%N.err # STDERR
+#SBATCH --gres=gpu:1
+if [ -f "/usr/local/anaconda3/etc/profile.d/conda.sh" ]; then
+    . "/usr/local/anaconda3/etc/profile.d/conda.sh"
+else
+    export PATH="/usr/local/anaconda3/bin:$PATH"
+fi
+cd ~/SSM_benchmark/HyperFisher/
+conda activate venv
+
+
+#if you want to run with single gpus, use the following command
+python main.py --task spit_mnist --methods ewc fopng adam --embedding_dim=4 --epochs=5 --fisher_samples=4096 --grads_per_task=80 --max_directions=400 --model=MLP
